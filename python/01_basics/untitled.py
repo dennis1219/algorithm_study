3c27811938688278e13caf28603eec9b5b7be4d3
dennis1219 <dennis011219@naver.com>
2021-07-25 00:00:20 +0900

    Update untitled.py

--- python/01_basics/untitled.py
+++ python/01_basics/untitled.py
@@ -1,9 +1,14 @@
 import sys
 import itertools
 import math
+import statistics
 
 a = sys.stdin.readline().rstrip()
 b,c = map(int, sys.stdin.readline().rstrip().split())
 
+l = list()
+
 for i in range(a):
     while True:
+        statistics.multimode('b','d','f')
+        statistics.median()
